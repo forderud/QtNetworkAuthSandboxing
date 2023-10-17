da609e621abe2be3b792e6ca9673e1f256ba5515
Fredrik Orderud <fredrik.orderud@ge.com>
2023-10-17 15:37:52 +0200

    Improve reproducer.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@
 #include <Windows.h>
 #pragma comment(lib, "Crypt32.lib")
 
-#define REPRODUCE_ROOT_CAUSE
+//#define REPRODUCE_ROOT_CAUSE
+//#define FIX_ROOT_CAUSE
 
-
+/** Reproducer for low integrity level (low IL) problem. */
 class HttpClient {
 public:
     HttpClient(QObject * parent) : m_mgr(parent) {
@@ -24,7 +25,7 @@ public:
         request.setMaximumRedirectsAllowed(0); // trigger failure on SAML login screens
         QNetworkReply* reply = m_mgr.get(request);
 
-        // block until the download have completed
+        // this line will deadlock
         loop.exec();
 
         auto err = reply->error();
@@ -41,13 +42,23 @@ private:
     QNetworkAccessManager m_mgr;
 };
 
-int main(int argc, char *argv[])
-{
+/** Steps to reproduce:
+* From an _administrator_ command prompt: Run `icacls QtNetworkAuthSandboxing.exe /setintegritylevel Low`.
+* From a _regular_ command prompt: Run `QtNetworkAuthSandboxing.exe`
+* Observe that the program freezes without any console output.
+*/
+int main(int argc, char *argv[]) {
 #ifdef REPRODUCE_ROOT_CAUSE
-    // call from https://github.com/qt/qtbase/blob/6.5/src/plugins/tls/schannel/qtls_schannel.cpp#L301
+    // failing call from https://github.com/qt/qtbase/blob/6.5/src/plugins/tls/schannel/qtls_schannel.cpp#L301
     HCERTSTORE store = CertOpenSystemStoreW(0, L"ROOT");
+    DWORD err = GetLastError(); // 5 = Access is denied.
+    assert(store);
+#elif defined FIX_ROOT_CAUSE
+    // possible fix
+    HCERTSTORE store = CertOpenStore(CERT_STORE_PROV_SYSTEM, 0, NULL, CERT_STORE_READONLY_FLAG | CERT_SYSTEM_STORE_CURRENT_USER, L"ROOT");
     assert(store);
 #else
+    // Full QNetworkAccessManager reproducer
     QCoreApplication a(argc, argv);
 
     HttpClient httpClient(nullptr);
